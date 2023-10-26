7ccfeb4b2b26e3ca1d9ca73561b1f6421945843e
samuel21695 <samuel21695@gmail.com>
2023-10-26 21:48:13 +0900

    develop start

---
+++ app.js
@@ -0,0 +1,15 @@
+// * 해당 코드는 '어떤 역할을 해낼까요?'
+// 유효성 검사를 하기 위해 dataTypeChecker라는 모듈화 되어있는 함수를 불러내었습니다. 
+// * 능력을 확인했다면, '어떤 절차'로 처리되었을까요?
+
+
+
+const fs = require('fs');
+const path = require('path');
+const dataTypeChecker = require('../../views/utility/datatype-checker');
+
+/**
+ * todo : 예제로 작성되어 있는 함수 bundleFiles() 복잡도가 매우 높다.
+ * 어떤 부분을 확인해야 적절하게 해당 로직을 이해할 수 있을까?
+ * 
+ */
